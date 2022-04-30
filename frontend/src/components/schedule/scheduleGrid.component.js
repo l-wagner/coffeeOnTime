52e52384880d560216327d256f889b17ee7ef7c3
Lisa Wagner <mail@lisawagner.io>
2022-04-30 16:10:19 -0400

    added row colors

--- frontend/src/components/schedule/scheduleGrid.component.js
+++ frontend/src/components/schedule/scheduleGrid.component.js
@@ -4,8 +4,25 @@ import { registerAllModules } from 'handsontable/registry';
 // register Handsontable's modules
 registerAllModules();
 
+const colors = [
+  'light-pink',
+  'deep-champagne',
+  'lemon-yellow-crayola',
+  'tea-green',
+  'electric-blue',
+  'baby-blue-eyes',
+  'maximum-blue-purple',
+  'mauve',
+  'baby-powder',
+];
 const settings = {
   licenseKey: 'non-commercial-and-evaluation',
+  cells(row, column) {
+    return {
+      // readOnly: true,
+      className: colors[row],
+    };
+  },
 };
 
 export default function ScheduleGrid(props) {
